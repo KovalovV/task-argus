5f396cd083d9a9630c250b1bd999cabf86d59975
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-03-18 14:49:25 +0200

    root component

---
+++ src/root/index.jsx
@@ -0,0 +1,11 @@
+import { InfoForm } from "components/form";
+
+import "./styles.scss";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <InfoForm />
+    </div>
+  );
+};

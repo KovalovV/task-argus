4f83cc34ef87a43edb623d175f430a844cb1883d
Vitala Kovalov <vitalya.kovalov@gmail.com>
2022-03-19 22:29:59 +0200

    other

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,10 +1,17 @@
+import dayjs from "dayjs";
+import "dayjs/locale/uk";
+
+import { Header } from "components/header";
 import { InfoForm } from "components/form";
 
 import "./styles.scss";
 
+dayjs.locale("uk");
+
 export const App = () => {
   return (
     <div className="container">
+      <Header />
       <InfoForm />
     </div>
   );
